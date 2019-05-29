ad01ac772fce88044de8977b3a41fe4dbd4410df
Cryosith <caseyvandyke@ymail.com>
2019-05-28 21:28:14 -0600

    random generation

--- src/QuoteMachine.js
+++ src/QuoteMachine.js
@@ -4,7 +4,8 @@ class QuoteMachine extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      quotes: []
+      quotes: [],
+      randomNumber: 0
     };
   }
 
@@ -18,22 +19,66 @@ class QuoteMachine extends Component {
     );
   }
 
+  handleClick = e => {
+    const Random = Math.floor(Math.random() * this.state.quotes.length);
+    console.log("random number", Random);
+    //console.log(this.state.quotes);
+    this.setState({
+      quoteChange: Random
+    });
+  };
+
+  handleSubmit = e => {};
+
   componentDidMount() {
     this.callAPI();
   }
+
+  /*
+
+  function random_item(items)
+{
+  
+return items[Math.floor(Math.random()*items.length)];
+     
+}
+
+var items = [254, 45, 212, 365, 2543];
+console.log(random_item(items));
+
+  */
   render() {
-    const { quotes } = this.state;
+    console.log(this.state.randomNumber);
+    console.log(this.state.quotes.length);
+
+    const { quotes, quoteChange } = this.state;
+    console.log(quotes);
     return (
       <div className="quote-container">
-        {quotes.map((quote, i) => (
-          <div className="quote-center" key={i}>
-            <h1 className="quote">{quote.quote}</h1>
-            <p className="quote-author">{quote.author}</p>
-          </div>
-        ))}
+        <h1>
+          {this.state.quotes.length
+            ? this.state.quotes[this.state.randomNumber].quote
+            : "Loading ..."}
+        </h1>
+        <button onClick={this.handleClick}>Next</button>
       </div>
     );
   }
 }
 
 export default QuoteMachine;
+
+/* <div className="quote-container">
+        {quotes.map((quote, i) => (
+          <div className="quote-center" key={i}>
+            <h1 className="quote">{quote.quote}</h1>
+            <p className="quote-author">{quote.author}</p>
+            <ul>
+              <li>twitter icon</li>
+              <li>tumbler icon</li>
+            </ul>
+          </div>
+        ))}
+      </div>
+
+      */
